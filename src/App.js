1001cba9cd6895ebb1fa37299260be3f94803615
Harrigovind <121058026+Harrigovind@users.noreply.github.com>
2024-06-10 14:39:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import HomePage from './components/HomePage/HomePage';
 import SearchPage from './components/SearchPage/searchPage';
